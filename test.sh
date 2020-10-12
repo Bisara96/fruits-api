b5983a6c62490c1a341084bc296d460b7a4fa5cf
Buwaneka96 <buwanekathilakasiri@gmail.com>
2020-10-13 02:52:39 +0530

    Updated test script

--- test.sh
+++ test.sh
@@ -4,7 +4,9 @@ echo $OUTPUT_DIR
 
 echo testing fruit-api
 
-cp usr/src/app/fruitapi-master/* $OUTPUT_DIR/release-binary
+ls usr/src/app/fruitapi-master/
+
+cp usr/src/app/fruitapi-master/app.js $OUTPUT_DIR/release-binary
 
 NEXT=$(date +%s)
 
